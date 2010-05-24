a640b278d7fa508714ee97aff6b5b4e37bb9502c
Jordan Brough <jordan@animoto.com>
2010-05-24 14:41:38 -0600

    retrofitted to suit my needs
    my needs:  find errors and all notices associated with any given error

--- lib/hoptoad-api.rb
+++ lib/hoptoad-api.rb
@@ -4,31 +4,31 @@ require 'httparty'
 module Hoptoad
   extend self
   attr_accessor :secure
-  
+
   class HoptoadError < StandardError; end
-  
+
   def account=(account)
     @account = account
   end
-  
+
   def account
     "#{protocol}://#{@account}.hoptoadapp.com"
   end
-  
+
   def auth_token=(token)
     @auth_token = token
   end
-  
+
   def auth_token
     @auth_token
   end
-  
+
   def protocol
     secure ? "https" : "http"
   end
-  
+
 end
 
 require 'hoptoad-api/version'
 require 'hoptoad-api/core_extensions'
-require 'hoptoad-api/client'
+require 'hoptoad-api/client.rb'
--- lib/hoptoad-api/client.rb
+++ lib/hoptoad-api/client.rb
@@ -1,4 +1,4 @@
-# Ruby lib for working with the Hoptoad API's XML interface.  
+# Ruby lib for working with the Hoptoad API's XML interface.
 # The first thing you need to set is the account name.  This is the same
 # as the web address for your account.
 #
@@ -6,81 +6,123 @@
 #
 # Then, you should set the authentication token.
 #
-#   Hoptoad.token = 'abcdefg'
+#   Hoptoad.auth_token = 'abcdefg'
 #
 # If no token or authentication info is given, a HoptoadError exception will be raised.
 #
+# If your account uses ssl then turn it on:
+#
+#   Hoptoad.secure = true
+#
 # For more details, check out the hoptoad docs at http://hoptoadapp.com/pages/api.
 #
-# Find errors
+# Find errors by id
 #
-#   Errors are paginated. You get 25 at a time.
-#   errors = Hoptoad::Error.find(:all)
+#   error = Hoptoad::Error.find(1234)
 #
-#   with pagination:
-#   Hoptoad::Error.find(:all, :params => { :page => 2 })
+# Find *all* notices by error_id
 #
-#   find individual error by ID
-#   Hoptoad::Error.find(44) 
+#   notices = Hoptoad::Notice.all(1234) # 1234 == error id
+#
+# Find notice by id + error_id
+#
+#   notice = Hoptoad::Notice.find(12345, 1234) # 12345 == notice id, 1234 == error id
 
 module Hoptoad
-  class Error
+  class Base
     include HTTParty
     format :xml
-  
-    @@collection_path = '/errors.xml'
-    @@individual_collection_path = '/errors/'
-    
-    def self.collection_path
-      @@collection_path
-    end
-  
-    def self.find(*args)
-      base_uri Hoptoad.account
-      default_params :auth_token => Hoptoad.auth_token
-      
-      check_configuration
-    
-      results = case args.first
-        when Fixnum
-          find_individual(args)
-        when :all
-          find_all(args)
-        else
-          raise HoptoadError.new('Invalid argument')
-      end
-    
-      raise HoptoadError.new('No results found.') if results.nil?
-      raise HoptoadError.new(results.errors.error) if results.errors
-      
-      results.group || results.groups
-    end
-  
-    def self.update(error, options)
-      check_configuration
-    
-      self.class.put("#{@collection_path}", options)
-    end
-  
-    private
-  
-    def self.check_configuration
-      raise HoptoadError.new('API Token cannot be nil') if default_options.nil? || default_options[:default_params].nil? || !default_options[:default_params].has_key?(:auth_token)
-      raise HoptoadError.new('Account cannot be nil') unless default_options.has_key?(:base_uri)
-    end
-  
-    def self.find_all(args)
-      options = args.extract_options!
-      Hashie::Mash.new(get("#{@@collection_path}", { :query => options }))
-    end
-  
-    def self.find_individual(args)
-      id = args.shift
-      options = args.extract_options!
-      hash = Hashie::Mash.new(response = get("#{@@individual_collection_path}#{id}.xml", { :query => options }))
-      raise HoptoadError.new('SSL should be enabled - use Hoptoad.secure = true in configuration') if response.code == 403
-      hash
-    end
 
+    class << self
+      private
+
+      def setup
+        base_uri Hoptoad.account
+        default_params :auth_token => Hoptoad.auth_token
+        check_configuration
+      end
+
+      def check_configuration
+        raise HoptoadError.new('API Token cannot be nil') if default_options.nil? || default_options[:default_params].nil? || !default_options[:default_params].has_key?(:auth_token)
+        raise HoptoadError.new('Account cannot be nil') unless default_options.has_key?(:base_uri)
+      end
+    end
+  end
+
+  class Error < Base
+    class << self
+      def find(id, options={})
+        setup
+
+        response = get(find_path(id), {:query => options})
+        if response.code == 403
+          raise HoptoadError.new('SSL should be enabled - use Hoptoad.secure = true in configuration')
+        end
+        hash = Hashie::Mash.new(response)
+        if hash.errors
+          raise HoptoadError.new(results.errors.error)
+        end
+
+        hash.group
+      end
+
+      private
+
+      def find_path(id)
+        "/errors/#{id}.xml"
+      end
+    end
+  end
+
+  class Notice < Base
+    class << self
+      def find(id, error_id, options={})
+        setup
+
+        response = get(find_path(id, error_id), {:query => options})
+        if response.code == 403
+          raise HoptoadError.new('SSL should be enabled - use Hoptoad.secure = true in configuration')
+        end
+        hash = Hashie::Mash.new(response)
+        if hash.errors
+          raise HoptoadError.new(results.errors.error)
+        end
+
+        hash.notice
+      end
+
+      def all(error_id, options={})
+        setup
+
+        page = 1
+        notices = []
+        while true
+          options[:page] = page
+          hash = Hashie::Mash.new(get(all_path(error_id), {:query => options}))
+          if hash.errors
+            raise HoptoadError.new(results.errors.error)
+          end
+          notice_stubs = hash.notices
+
+          notice_stubs.map do |notice|
+            notices << find(notice.id, error_id)
+            puts "found #{notices.last.id}"
+          end
+          break if notice_stubs.size < 30
+          page += 1
+        end
+        notices
+      end
+
+      private
+
+      def find_path(id, error_id)
+        "/errors/#{error_id}/notices/#{id}.xml"
+      end
+      
+      def all_path(error_id)
+        "/errors/#{error_id}/notices.xml"
+      end
+    end
   end
 end
